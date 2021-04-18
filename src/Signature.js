2c437f4e55d8475095480c0ca898d48e9db67caf
Vilde Margrethe S. S <45592914+VildeMargrethe@users.noreply.github.com>
2021-04-18 17:45:24 +0200

    Fixed signature

--- src/Signature.js
+++ src/Signature.js
@@ -5,7 +5,7 @@ export default function Signature() {
   return (
     <p className="signature">
       <a href="https://github.com/VildeMargrethe/weather-app-react" target="_blank">
-        Open source code  
+        Open source code{" "}
       </a>
       by Vilde M. S. Strand.
     </p>
